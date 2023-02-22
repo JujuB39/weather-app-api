b768140b96f9e34c57d5ddfc995bb54d02b42c4d
Julissa Bonilla <julissab9@gmail.com>
2023-02-22 01:00:16 -0600

    working fetch apis

--- script.js
+++ script.js
@@ -11,21 +11,28 @@ var dayThree = document.getElementById("day-three");
 var dayFour = document.getElementById("day-four");
 var dayFive = document.getElementById("day-five");
 var pastSearch = document.querySelector('.past-search');
-var currentDate = moment().format('MMM DD YYYY, hh:mm A');
+var currentDate = moment().format('MMM DD YYYY');
 
-// search Information
-var cityName = '';
-console.log(cityName)
+var pastSearchArray = [];
 
 // Store city name into Search History local storage, pushing in citynames to local storage
 var formSubmit = function (event) {
     event.preventDefault();
     //Get search value 
-     // pastSearch.push(cityName)
-    // localStorage.getItem('Previous Search', JSON.stringify(pastSearch))
-    // localStorage.getItem("City", cityName)
-    // Store city name into Search History local storage
+    var city = inputEl.value;
+    pastSearchArray.push(city);
+    localStorage.setItem('Previous Search', JSON.stringify(pastSearchArray))
+    localStorage.setItem("City", city)
     handleSearch()
+
+    pastSearch.innerHTML = "Previous Search"
+    var previousCities = document.createElement("ul")
+    pastSearchArray.forEach((city) => {
+        var cityList = document.createElement("li");
+        cityList.textContent = city;
+        previousCities.appendChild(cityList);
+    })
+    pastSearch.appendChild(previousCities)
 }
 
 //fetch api
@@ -71,6 +78,7 @@ function getFiveDayForecast (latitude, longitude) {
     })
 
     .then(function(data) {
+        let i = 7;
         [dayOne, dayTwo, dayThree, dayFour, dayFive].forEach((day) => {
             var dateHeader = document.createElement("h4");
             var icon = document.createElement("img");
@@ -78,87 +86,31 @@ function getFiveDayForecast (latitude, longitude) {
             var humidity = document.createElement("p");
             var windspeed = document.createElement("p");
 
-            dateHeader.textContent = currentDate
-            icon.src = `https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`
-            temperature.textContent = `Temperature: ${data.main.temp} °F`
-            humidity.textContent = `Humidity: ${data.main.humidity} %`;
-            windspeed.textContent = `Windspeed: ${data.wind.speed} MPH`;
+            dateHeader.textContent = data.list[i].dt_txt.split(" ")[0];
+            icon.src = `https://openweathermap.org/img/wn/${data.list[i].weather[0].icon}@2x.png`
+            temperature.textContent = `Temperature: ${data.list[i].main.temp} °F`
+            humidity.textContent = `Humidity: ${data.list[i].main.humidity} %`;
+            windspeed.textContent = `Windspeed: ${data.list[i].wind.speed} MPH`;
+
+            i += 8;
+
+            //append to each day 
+            day.append(dateHeader);
+            day.append(temperature);
+            day.append(humidity);
+            day.append(windspeed);
+            day.append(icon);
 
         })
-
-
-
-        
-        
-
-        // //append to current weather
-        // currentForecast.append(cityHeader);
-        // currentForecast.append(dateHeader);
-        // currentForecast.append(temperature);
-        // currentForecast.append(humidity);
-        // currentForecast.append(windspeed);
-        // currentForecast.append(icon);
-
-    })
-}
-//FOr future 5 day forecasts
-// var cityandDateHeader = document.createElement("h3");
-// var icon = document.createElement("img");
-// var temperature = document.createElement("p");
-// var Humidity = document.createElement("p");
-// var Windspeed = document.createElement("p"); 
-      //Need  /date 
-        // Need Icon representation
-        // Need Temp
-        //Need Humidity
-        // Need wind speed 
-
-      //is it possible to loop through each data in the dom and create the same elements ASK????
       
-      //then ask if you can also loop through text content as well not sure??? 
+    })
 
-
-
-        //Need City Name (name) /date 
-            
-        // Need Icon representation
-            //weather[0].icon
-        // Need Temp
-            //data.main.temp
-        //Need Humidity
-            //data.main.humidity
-        // Need wind speed 
-            //data.wind.speed
-
-
-
-        //Create Elements needed 
-
-        //var city = 
-
-
-
-
+}
 
 
 function handleSearch() {
     getForecast(inputEl.value);
 }
-
-
-   
-// function for search handler
-// function searchHandler(event) {
-//     event.preventdefault()
-//     var location = input.value.trim()
-//     pastSearchCities.push(location)
-//     localStorage.getItem("cities", JSON.stringify(pastSeach))
-//     localStorage.getItem("city", location)
-// }
-
-//function to renderpastcities 
-
-
 
 
 form.addEventListener('submit', formSubmit)
